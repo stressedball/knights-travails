b3a4efb00d8915d327e32e2d06270e559cdbc4db
stressedball <tserchesi@gmail.com>
2022-12-22 17:30:33 +0100

    Making a 8⁶³ tree at first seemed fun but my pc didnt let me do it unfortunately. Make use of BDS to get to the shortest path quickly. Interested now in getting a mathematical clue of the min moves for any board tile. Didnt test thoroughly.

--- index.js
+++ index.js
@@ -1,37 +1,103 @@
 'use strict';
-
 // we store all the possible moves the knight can make
-const possibleMoves = [[-1, 2], [-2, 1], [-2, -1], [-1, -2],
+const moves = [[-1, 2], [-2, 1], [-2, -1], [-1, -2],
                 [1, -2], [2, -1], [2, 1], [1, 2]];
 const startPoint = [0, 0]; // start with an example
-const target = [3, 3]; // first target example
-
-class Tree {
-    constructor(start) {
-        this.root = buildTree(start);
+const target = [6, 3]; // first target example
+class Queue {
+    constructor() {
+        this.queue = [];
+    }
+    enqueue([x, y]) {
+        this.data = {
+            x: x,
+            y: y,
+            visited: [],
+        }
+        this.queue.push(this.data);
+    }
+    getQueue() {
+        return this.queue;
+    }
+    dequeue() {
+        this.queue.splice(0, 1);
+    }
+    head() {
+        return this.queue[0];
+    }
+    tail() {
+        return this.queue[this.queue.length - 1];
     }
 }
-function knightMoves(start, end) {
-    let tree = new Tree(start);
-}
-function buildTree([x, y]) {
-    // get the next moves
-    // put them in an array
-    // put the array as child of the coordinates
-    // problem 1 : start is an array and not an object
-    let moves = getMoves([x, y]);
-    // basic structure
-    return;
-}
-const getMoves = ([x, y]) => {
-    let arr = [];
-    for (let move of possibleMoves) {
-        let f_x = x + move[0]; let f_y = y + move[1];
-        if (f_x > 7 || f_y > 7 || f_x < 0 || f_y < 0) continue;
-        arr.push([f_x, f_y]);
+function knightMoves(start, target) {
+    let path = findShortestPath(start, target);
+    for (let el of path) {
+        console.log(el)
     }
-    return arr;
-} 
+}
+function findShortestPath(start, target) {
+    let xS = start[0]; 
+    let yS = start[1];
+    let xT = target[0];
+    let yT = target[1];
+    let queue = new Queue();
 
+    queue.enqueue([xS, yS]);
+
+    while (queue.getQueue().length > 0) {
+        let x = queue.head().x; 
+        let y = queue.head().y;
+        for (let i = 0; i < moves.length; i++) {
+            let xNext = moves[i][0]; 
+            let yNext = moves[i][1];
+            let nextMove = getNextMove([x, y], [xNext, yNext]);
+            if (nextMove === null) continue;
+            if (checkVisited([x, y], queue.head().visited) === true) continue;
+
+            let xCalc = nextMove[0]; let yCalc = nextMove[1];
+            if (checkTarget([xCalc, yCalc], [xT, yT]) === true) {
+                if (queue.head().visited.length === 0) return [x, y].concat([xCalc, yCalc]);
+                let path = queue.head().visited;
+                path.push([x, y]);
+                path.push([xT, yT]);
+                return path;
+            }
+
+            queue.enqueue([xCalc, yCalc]);
+            if (queue.head().visited.length === 0) {
+                queue.tail().visited.push([x, y]);
+            } else {
+                getVisited(queue.head().visited, queue.tail());
+                queue.tail().visited.push([x, y])
+            }
+        }
+        queue.dequeue();
+    }
+}
+const getNextMove = ([xS, yS], [xNext, yNext]) => {
+    let xCalc = xS + xNext; 
+    let yCalc = yS + yNext;
+    if (xCalc > 7 || yCalc > 7 || xCalc < 0 || yCalc < 0) return null;
+    return [xCalc, yCalc];
+}
+const checkTarget = ([x, y], [xT, yT]) => {
+    if (x === xT && y === yT) return true;
+    return false;
+}
+const checkVisited = ([x, y], visited) => {
+    for (let i = 0; i < visited.length; i++) {
+        let xV = visited[i][0]; 
+        let yV = visited[i][1];
+        if (xV === x && yV === y) return true;
+    }
+    return false;
+}
+const getVisited = (array, object) => {
+    for (let i = 0; i < array.length; i++) {
+        let x = array[i][0];
+        let y = array[i][1];
+        object.visited.push([x, y]);
+    }
+}
 knightMoves(startPoint, target);
 
